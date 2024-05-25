3db0d17d8ad1b9a2ccb1616299bf1752fc34c574
AngelicaCahum2525 <monsecahuum@gmail.com>
2024-05-24 20:29:45 -0600

    Se agrego el footer y la vista de registro

--- routes/routes.js
+++ routes/routes.js
@@ -4,14 +4,14 @@ const router = express.Router();
 // Importa las rutas específicas
 const home = require('./home');
 const login = require('./login');
-//const registro = require('./registro');
+const registro = require('./register');
 //const registrarUsuario = require('./registrar-usuario');
 
 
 //Configura las rutas
 router.use('/', home);
 router.use('/login', login);
-//router.use('/registro', registro);
+router.use('/registro', registro);
 //router.use('/registrar-usuario', registrarUsuario);
 
 module.exports = router;
