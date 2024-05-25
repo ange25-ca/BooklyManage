89cbaab3fcbde1fa85b3e9378ac9588a269b2c5d
AngelicaCahum2525 <monsecahuum@gmail.com>
2024-05-25 00:25:30 -0600

    vista de buscar (sin funcionalidad)

--- routes/routes.js
+++ routes/routes.js
@@ -4,14 +4,16 @@ const router = express.Router();
 // Importa las rutas específicas
 const home = require('./home');
 const login = require('./login');
-const registro = require('./register');
+const register = require('./register');
+const search = require('./search');
 //const registrarUsuario = require('./registrar-usuario');
 
 
 //Configura las rutas
 router.use('/', home);
 router.use('/login', login);
-router.use('/registro', registro);
+router.use('/register', register);
+router.use('/search', search);
 //router.use('/registrar-usuario', registrarUsuario);
 
 module.exports = router;
